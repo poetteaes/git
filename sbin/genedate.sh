cd991893d650a3a96abcbe21fcb055dd6203b268
poetteaes <caofei_kevin@163.com>
2018-04-15 14:46:20 +0800

    genedate.sh

--- sbin/genedate.sh
+++ sbin/genedate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
   
-#以YYYYMMDD格式输出大于等于第一个输入日期，小于第二个输入日期的所有日期
+#以YYYYMMDD格式输出大于等于第一个输入日期，小于等于二个输入日期的所有日期
   
 begin_date=$1  
 end_date=$2
@@ -10,4 +10,4 @@ do
 echo $begin_date  
 let begin_date=`date -d "-1 days ago ${begin_date}" +%Y%m%d` 
 done
-
+echo $end_date 
